ca799ee82580d9482e663111244c3b64f0f7de8b
ashraful-rahat <spinball61@gmail.com>
2025-08-18 12:33:55 +0600

    d

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // env থেকে নেবে
+  baseURL: process.env.NEXT_PUBLIC_API_URL, 
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
 });
